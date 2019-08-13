1b8fa08106aa0292e04cd91d84d510cdfd8c3740
v_liupeng <system_lp@126.com>
2019-08-13 16:31:30 +0800

    首页banner

--- src/index/indexBanner.js
+++ src/index/indexBanner.js
@@ -1,39 +1,46 @@
 import React, { Component } from "react";
+import { Link } from "react-router";
 import { Carousel } from "antd";
+import { fetchUtil } from "../utils/FetchUtil";
 
 export default class IndexBanner extends Component {
+  state = {};
+  findBanners() {
+    fetchUtil({
+      url: "/store/index/banner",
+      callback: ({ data: banners }) => {
+        if (banners) {
+          this.setState({ banners });
+        }
+      }
+    });
+  }
+  componentWillMount() {
+    this.findBanners();
+  }
   render() {
+    const { banners = [] } = this.state;
     return (
-      <div className="index-banner-warp">
-        <Carousel autoplay effect="fade">
-          <div>
-            <img
-              className="bannerImg"
-              alt="banner"
-              src="https://img.alicdn.com/imgextra/i2/29/O1CN01eI9T9x1C5K2AkG6Yo_!!29-0-luban.jpg_q100.jpg_.webp"
-            />
-          </div>
-          <div>
-            <img
-              className="bannerImg"
-              alt="banner"
-              src="https://img.alicdn.com/imgextra/i2/29/O1CN01eI9T9x1C5K2AkG6Yo_!!29-0-luban.jpg_q100.jpg_.webp"
-            />
-          </div>
-          <div>
-            <img
-              className="bannerImg"
-              alt="banner"
-              src="https://img.alicdn.com/imgextra/i2/29/O1CN01eI9T9x1C5K2AkG6Yo_!!29-0-luban.jpg_q100.jpg_.webp"
-            />
-          </div>
-          <div>
-            <img
-              className="bannerImg"
-              alt="banner"
-              src="https://img.alicdn.com/imgextra/i2/29/O1CN01eI9T9x1C5K2AkG6Yo_!!29-0-luban.jpg_q100.jpg_.webp"
-            />
-          </div>
+      <div id="index-banner-warp" className="index-banner-warp">
+        <Carousel
+          autoplay
+          effect="fade"
+          beforeChange={(
+            from,
+            to // 异步事件会导致无法手动切换banner
+          ) =>
+            (document.getElementById(
+              "index-banner-warp"
+            ).style.backgroundColor = `#${banners[to].bannerColor}`)
+          }
+        >
+          {banners.map(({ storeId, bannerImg }) => (
+            <div key={storeId}>
+              <Link to={`/store/${storeId}`}>
+                <img className="bannerImg" alt="banner" src={bannerImg} />
+              </Link>
+            </div>
+          ))}
         </Carousel>
       </div>
     );
